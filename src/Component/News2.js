32045cd5d77ca8d7da302c850ff8b24f9aa55266
DareIsrael <dareisrael4@gmail.com>
2023-09-28 03:36:08 -0700

    please work

--- src/Component/News2.js
+++ src/Component/News2.js
@@ -1,71 +1,18 @@
-// import React, { useState, useEffect } from 'react';
-
-
-
-
-// function ShipmentNews() {
-//   const [news, setNews] = useState([]);
-  
-// const apiKey = "3ee11dc7fe9d40b199e62099f007be6c"
-
-//   useEffect(() => {
-//     const fetchNews = async () => {
-//       try {
-//         const response = await fetch(
-//           `https://newsapi.org/v2/everything?q=shipment&apiKey=${apiKey}`
-//         );
-
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-
-//         const data = await response.json();
-//         setNews(data.articles);
-//       } catch (error) {
-//         console.error('Error fetching news:', error);
-//       }
-//     };
-
-//     fetchNews();
-//   }, []);
-
-//   return (
-//     <div className='news1'>
-//       <h1>Shipment News</h1>
-//       <ul className='news2'>
-//         {news.map((article, index) => (
-//           <li key={index}>
-//             <a href={article.url} target="_blank" rel="noopener noreferrer">
-//               <img
-//                 src={article.urlToImage}
-//                 alt={article.title}
-//                 width="100"
-//                 height="100"
-//               />
-//               {article.title}
-//             </a>
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
-// export default ShipmentNews;
-// "7934a387e916feb02b3787743e1aafce";
-
-
 import React, { useState, useEffect } from 'react';
+
+
+
 
 function ShipmentNews() {
   const [news, setNews] = useState([]);
-  const apiKey = "7934a387e916feb02b3787743e1aafce"; // Replace with your GNews API key
+  
+const apiKey = "3ee11dc7fe9d40b199e62099f007be6c"
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
         const response = await fetch(
-          `https://gnews.io/api/v4/top-headlines?q=shipment&token=${apiKey}`
+          `https://newsapi.org/v2/everything?q=shipment&apiKey=${apiKey}`
         );
 
         if (!response.ok) {
@@ -90,7 +37,7 @@ function ShipmentNews() {
           <li key={index}>
             <a href={article.url} target="_blank" rel="noopener noreferrer">
               <img
-                src={article.image}
+                src={article.urlToImage}
                 alt={article.title}
                 width="100"
                 height="100"
@@ -105,3 +52,7 @@ function ShipmentNews() {
 }
 
 export default ShipmentNews;
+// "7934a387e916feb02b3787743e1aafce";
+
+
+// 
