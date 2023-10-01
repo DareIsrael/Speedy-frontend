1a903870dbfbc8bbfffe7440a67350ad38441059
DareIsrael <dareisrael4@gmail.com>
2023-10-01 08:31:55 -0700

    adding api

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -13,7 +13,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError (null)
 
-        const response = await fetch ('api/user/signup', {
+        const response = await fetch ('https://speedyspell-api.onrender.com/api/user/signup', {
             method: 'POST',
             headers: {'Content-Type' : 'application/json'},
             body: JSON.stringify({fName, lName, cName, phone, sAddress,
